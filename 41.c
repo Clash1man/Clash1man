e9e2e01f7282529bd4aff59b806abf37d9455543
Clash1man <136419726+Clash1man@users.noreply.github.com>
2024-03-25 13:00:09 +0545

    41.c

---
+++ 41.c
@@ -0,0 +1,37 @@
+include stdio.h
+
+Lionio.h> ti Include 210
+
+Struct P
+
+S
+
+Int ID;
+
+char name[50], add[50]; 3
+
+0bJE5];
+
+vold maint
+
+7
+
+int i, for cl=0; 125; i++
+
+printf ("In Enter ID').
+
+scanf("%d BobJEJ.ID); Printf ("In Enter Nume")." StanfCs obsti].namel; printf("in Enter address). scanfcrs", obj[i].addit
+
+3
+
+Printf ("In ID It frame It address"); for C1=0; 125; 4+1
+
+Σ
+
+Pintf("In dit "SIES It", obj[i]-ld, Obj[i].hamp, obsti].add!;
+
+3
+
+getch U:
+
+3
